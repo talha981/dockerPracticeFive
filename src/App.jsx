e52950d2a14cfa496c7e4ae88c66fcb2d0838ae6
talha981 <safdartalha69@gmail.com>
2025-08-02 13:00:59 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to My App Its Day six of learning devops</h1>
+      <p>This is a simple React application.</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa id, placeat architecto quod fugit repellat maiores corporis ut quia reprehenderit aperiam magni reiciendis quidem quae accusamus veniam facere ipsum molestias.</p>
+    </div>
+  );
+}
