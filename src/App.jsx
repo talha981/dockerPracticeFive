939feb1e39cd54152535582830c081f910dbc215
talha981 <safdartalha69@gmail.com>
2025-08-02 20:38:30 +0500

    shared library

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1>Welcome to My App Its Day six of learning devops</h1>
-      <h2>Automate Github Push  added </h2>
+      <h1>Welcome to My App Its Day six of learning devops </h1>
+      <h2>Automate Github Push  added and also shared library </h2>
       <p>This is a simple React application.</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa id, placeat architecto quod fugit repellat maiores corporis ut quia reprehenderit aperiam magni reiciendis quidem quae accusamus veniam facere ipsum molestias.</p>
     </div>
