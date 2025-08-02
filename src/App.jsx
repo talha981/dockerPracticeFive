0e5d8917e758b4a6e1f414604a98b00704e8d811
talha981 <safdartalha69@gmail.com>
2025-08-02 20:00:11 +0500

    updated github feature

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <div>
       <h1>Welcome to My App Its Day six of learning devops</h1>
+      <h2>Automate Github Push Added </h2>
       <p>This is a simple React application.</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa id, placeat architecto quod fugit repellat maiores corporis ut quia reprehenderit aperiam magni reiciendis quidem quae accusamus veniam facere ipsum molestias.</p>
     </div>
