6cb5df8e9db4822ef533ffe2720f1d76d4f2ed66
talha981 <safdartalha69@gmail.com>
2025-08-03 13:09:30 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1>Welcome to My App Its Day six of learning devops </h1>
+      <h1>Welcome to My App Its Day Eight of learning devops </h1>
       <h2>Automate Github Push  added and also shared library + commit code </h2>
       <p>This is a simple React application.</p>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsa id, placeat architecto quod fugit repellat maiores corporis ut quia reprehenderit aperiam magni reiciendis quidem quae accusamus veniam facere ipsum molestias.</p>
